e0c3aebb1b94207e1ee32a5f186b0b5dc26ce2f8
Diego Jordao <jordao.diego@yahoo.com>
2022-10-31 16:57:39 -0300

    updating files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Header from './components/Header/Header';
 import MenuMobile from './components/MenuMobile/MenuMobile';
-import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
 import './App.css';
+import Routes from './components/Routes/Routes'
 
 function App() {
   return (    
     <div>
       <MenuMobile/>
       <Header />
-      <Home />
+      <Routes />
       <Footer />
     </div>
     
